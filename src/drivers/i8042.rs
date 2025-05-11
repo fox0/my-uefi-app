758af0f5dec076c95a9494e84edf3b3b9ed242da
fox0 <15684995+fox0@users.noreply.github.com>
2025-05-11 15:40:30 +0700

    set_controller_configuration_byte

--- src/drivers/i8042.rs
+++ src/drivers/i8042.rs
@@ -1,10 +1,6 @@
 #![cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 
 //! I8042 PS/2 Controller
-//!
-//! * [`CommandRegister`]
-//! * [`StatusRegister`]
-//! * [`DataPort`]
 
 // https://wiki.osdev.org/I8042_PS/2_Controller
 
@@ -51,8 +47,15 @@ impl Driver for I8042 {
         while port::PortDataPort::read().is_some() {}
 
         // Step 5: Set the Controller Configuration Byte
-        let t = port::PortCommandRegister::get_controller_configuration_byte();
-        log::debug!("{:?}", t);
+        let mut config = port::PortCommandRegister::get_controller_configuration_byte();
+        // log::debug!("{:?}", config);
+        config.set_first_port_interrupt_is_enable(false);
+        config.set_second_port_interrupt_is_enable(false);
+        config.set_first_port_clock_disabled(true);
+        config.set_second_port_clock_disabled(true);
+        port::PortCommandRegister::set_controller_configuration_byte(config);
+        // let config = port::PortCommandRegister::get_controller_configuration_byte();
+        // log::debug!("{:?}", config);
 
         // todo!()
     }
@@ -85,9 +88,13 @@ mod dto {
     #[allow(dead_code)]
     #[repr(u8)]
     pub enum Commands {
-        /// Read "byte 0" from internal RAM
+        /// Read "byte 0" from internal RAM [`ControllerConfigurationByte`]
         ReadByte0 = 0x20,
-        // ...
+        // 0x21 to 0x3F Read "byte N" from internal RAM (where 'N' is the command byte & 0x1F).
+        // Response Byte: Unknown (only the first byte of internal RAM has a standard purpose)
+        /// Write next byte to "byte 0" of internal RAM  [`ControllerConfigurationByte`]
+        WriteByte0 = 0x60,
+        // 0x61 to 0x7F Write next byte to "byte N" of internal RAM (where 'N' is the command byte & 0x1F)
         /// Disable second PS/2 port (only if 2 PS/2 ports supported)
         DisableSecondPort = 0xA7,
         /// Enable second PS/2 port (only if 2 PS/2 ports supported)
@@ -109,26 +116,26 @@ mod dto {
 impl port::PortCommandRegister {
     const PORT: PortGeneric<u8, WriteOnlyAccess> = PortWriteOnly::<u8>::new(0x0064);
 
-    pub fn disable_first_port() {
-        Self::run_cmd(dto::Commands::DisableFirstPort);
-        // Response Byte: None
-    }
-
-    pub fn disable_second_port() {
-        Self::run_cmd(dto::Commands::DisableSecondPort);
-        // Response Byte: None
-    }
-
     pub fn get_controller_configuration_byte() -> dto::ControllerConfigurationByte {
-        Self::run_cmd(dto::Commands::ReadByte0);
+        unsafe { Self::PORT.write(dto::Commands::ReadByte0.into()) };
         let resp = port::PortDataPort::read().unwrap();
         dto::ControllerConfigurationByte(resp)
     }
 
-    fn run_cmd(cmd: dto::Commands) {
-        unsafe {
-            Self::PORT.write(cmd.into());
-        }
+    pub fn set_controller_configuration_byte(config: dto::ControllerConfigurationByte) {
+        unsafe { Self::PORT.write(dto::Commands::WriteByte0.into()) };
+        port::PortDataPort::write(config.into());
+        // Response Byte: None
+    }
+
+    pub fn disable_first_port() {
+        unsafe { Self::PORT.write(dto::Commands::DisableFirstPort.into()) };
+        // Response Byte: None
+    }
+
+    pub fn disable_second_port() {
+        unsafe { Self::PORT.write(dto::Commands::DisableSecondPort.into()) };
+        // Response Byte: None
     }
 }
 
@@ -137,7 +144,9 @@ impl port::PortStatusRegister {
 
     pub fn read() -> dto::StatusRegister {
         // log::trace!("read from port 0x0064");
-        dto::StatusRegister(unsafe { Self::PORT.read() })
+        let status = dto::StatusRegister(unsafe { Self::PORT.read() });
+        // log::debug!("{:?}", status);
+        status
     }
 }
 
@@ -151,6 +160,10 @@ impl port::PortDataPort {
         } else {
             None
         }
+    }
+
+    fn write(value: u8) {
+        unsafe { Self::PORT.write(value) };
     }
 }
 
@@ -179,7 +192,26 @@ impl dto::StatusRegister {
         self.0.get_bit(2)
     }
 
-    // TODO
+    /// Command/data (0 = data written to input buffer is data for PS/2 device, 1 = data written to input buffer is data for PS/2 controller command)
+    pub fn is_command(&self) -> bool {
+        self.0.get_bit(3)
+    }
+
+    // 4 Unknown (chipset specific)
+    // May be "keyboard lock" (more likely unused on modern systems)
+
+    // 5 Unknown (chipset specific)
+    // May be "receive time-out" or "second PS/2 port output buffer full"
+
+    /// Time-out error (0 = no error, 1 = time-out error)
+    pub fn is_timeout_error(&self) -> bool {
+        self.0.get_bit(6)
+    }
+
+    /// Parity error (0 = no error, 1 = parity error)
+    pub fn is_parity_error(&self) -> bool {
+        self.0.get_bit(7)
+    }
 }
 
 impl fmt::Debug for dto::StatusRegister {
@@ -188,10 +220,14 @@ impl fmt::Debug for dto::StatusRegister {
             .field("output_buffer_is_full", &self.output_buffer_is_full())
             .field("input_buffer_is_full", &self.input_buffer_is_full())
             .field("system_flag", &self.system_flag())
+            .field("is_command", &self.is_command())
+            .field("is_timeout_error", &self.is_timeout_error())
+            .field("is_parity_error", &self.is_parity_error())
             .finish()
     }
 }
 
+#[allow(dead_code)]
 impl dto::ControllerConfigurationByte {
     /// First PS/2 port interrupt (1 = enabled, 0 = disabled)
     pub fn first_port_interrupt_is_enable(&self) -> bool {
@@ -208,7 +244,54 @@ impl dto::ControllerConfigurationByte {
         self.0.get_bit(2)
     }
 
-    // TODO
+    // 3 Should be zero
+
+    /// First PS/2 port clock (1 = disabled, 0 = enabled)
+    pub fn first_port_clock_disabled(&self) -> bool {
+        self.0.get_bit(4)
+    }
+
+    /// Second PS/2 port clock (1 = disabled, 0 = enabled, only if 2 PS/2 ports supported)
+    pub fn second_port_clock_disabled(&self) -> bool {
+        self.0.get_bit(5)
+    }
+
+    /// First PS/2 port translation (1 = enabled, 0 = disabled)
+    pub fn first_port_translation_enabled(&self) -> bool {
+        self.0.get_bit(6)
+    }
+
+    // 7 Must be zero
+
+    /// First PS/2 port interrupt (1 = enabled, 0 = disabled)
+    pub fn set_first_port_interrupt_is_enable(&mut self, value: bool) {
+        self.0.set_bit(0, value);
+    }
+
+    /// Second PS/2 port interrupt (1 = enabled, 0 = disabled, only if 2 PS/2 ports supported)
+    pub fn set_second_port_interrupt_is_enable(&mut self, value: bool) {
+        self.0.set_bit(1, value);
+    }
+
+    /// System Flag (1 = system passed POST, 0 = your OS shouldn't be running)
+    pub fn set_system_flag(&mut self, value: bool) {
+        self.0.set_bit(2, value);
+    }
+
+    /// First PS/2 port clock (1 = disabled, 0 = enabled)
+    pub fn set_first_port_clock_disabled(&mut self, value: bool) {
+        self.0.set_bit(4, value);
+    }
+
+    /// Second PS/2 port clock (1 = disabled, 0 = enabled, only if 2 PS/2 ports supported)
+    pub fn set_second_port_clock_disabled(&mut self, value: bool) {
+        self.0.set_bit(5, value);
+    }
+
+    /// First PS/2 port translation (1 = enabled, 0 = disabled)
+    pub fn set_first_port_translation_enabled(&mut self, value: bool) {
+        self.0.set_bit(6, value);
+    }
 }
 
 impl fmt::Debug for dto::ControllerConfigurationByte {
@@ -223,6 +306,24 @@ impl fmt::Debug for dto::ControllerConfigurationByte {
                 &self.second_port_interrupt_is_enable(),
             )
             .field("system_flag", &self.system_flag())
+            .field(
+                "first_port_clock_disabled",
+                &self.first_port_clock_disabled(),
+            )
+            .field(
+                "second_port_clock_disabled",
+                &self.second_port_clock_disabled(),
+            )
+            .field(
+                "first_port_translation_enabled",
+                &self.first_port_translation_enabled(),
+            )
             .finish()
+    }
+}
+
+impl From<dto::ControllerConfigurationByte> for u8 {
+    fn from(value: dto::ControllerConfigurationByte) -> Self {
+        value.0
     }
 }
